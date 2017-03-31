f7b5560ea3b6757d9f5c3473d6389760371f2657
Yang Yu <yangyangloveyy@126.com>
2017-03-31 19:33:13 +0800

    31 commit

--- runner.py
+++ runner.py
@@ -1,6 +1,8 @@
-import login
-import createSchedule,logout
 import unittest
+import loginPro, logoutPro
+import createTaskPro
+import createSchedulePro
+
 
 # mysuite = unittest.TestSuite()
 # mysuite.addTest(loginsucc.MyTestCase("testLogIn"))
@@ -9,19 +11,19 @@ import unittest
 
 
 # 执行登录
-logincases = unittest.TestLoader().loadTestsFromTestCase(login.MyTestCase)
-# 执行创建任务并调度
-createSchedulecases = unittest.TestLoader().loadTestsFromTestCase(createSchedule.MyTestCase)
+logincases = unittest.TestLoader().loadTestsFromTestCase(loginPro.MyTestCase )
+'''业务'''
+# 执行创建任务
+createTaskcases = unittest.TestLoader().loadTestsFromTestCase(createTaskPro.MyTestCase )
+# 执行创建调度
+createDeliverycases = unittest.TestLoader().loadTestsFromTestCase(createSchedulePro.MyTestCase )
 # 注销
-logutcases = unittest.TestLoader().loadTestsFromTestCase(logout.MyTestCase)
+logutcases = unittest.TestLoader().loadTestsFromTestCase(logoutPro.MyTestCase )
 
-
-mysuite = unittest.TestSuite([logincases, createSchedulecases, logutcases])
+mysuite = unittest.TestSuite([logincases, createTaskcases, createDeliverycases, logutcases])
 
 # mysuite.addTest(unittestdemo.MyTestCase("testLogIn"))
 # mysuite.addTest(pickup.MyTestCase("testPickup"))
-
-
 
 myrunner = unittest.TextTestRunner(verbosity=2)
 myrunner.run(mysuite)
